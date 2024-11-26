96dc94cc84a27b613172669d559ec5dd5f3d057e
Dima <aldmikon@gmail.com>
2024-11-26 14:26:16 +0800

    Fixing a bug with reading env variables type of int

--- settings.py
+++ settings.py
@@ -6,7 +6,8 @@ load_dotenv()
 BASE_URL = os.getenv("BASE_URL")
 CATEGORIES = os.getenv("CATEGORIES")
 
-TIMEOUT = os.getenv("TIMEOUT")
-DELAY = os.getenv("DELAY")
-MAX_RETRIES = os.getenv("MAX_RETRIES")
-MAX_WORKERS = os.getenv("MAX_WORKERS")
+TIMEOUT = int(os.getenv("TIMEOUT"))
+DELAY = int(os.getenv("DELAY"))
+MAX_RETRIES = int(os.getenv("MAX_RETRIES"))
+MAX_WORKERS = int(os.getenv("MAX_WORKERS"))
+
