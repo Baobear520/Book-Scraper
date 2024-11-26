f1492c357cd71bf3a4c8ed30988ad0ca06b432f8
Dima <aldmikon@gmail.com>
2024-11-26 14:22:48 +0800

    Adding exceptions.py

---
+++ other_scripts/exceptions.py
@@ -0,0 +1,8 @@
+
+
+class MaxRetriesExceeded(Exception):
+    """Custom exception raised when maximum retries are reached."""
+    def __init__(self, url, message="Maximum retries reached"):
+        self.url = url
+        self.message = message
+        super().__init__(f"{message} for URL: {url}")
